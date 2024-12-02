e36a918fceac862a5a5cd3a03a53fa63c2a7d567
mightyking-ryu <dongwooryu@snu.ac.kr>
2024-12-02 15:19:02 +0900

    Implement get_length module

--- src/rtl/get_length.v
+++ src/rtl/get_length.v
@@ -15,7 +15,42 @@ output md_end;
 
 	//Implement get_length module
 
+	reg [63:0] num_reg;
+	reg [7:0] len_reg;
+	reg md_end_reg;
 
+	assign len_out = len_reg;
+	assign md_end = md_end_reg;
 
+	always @(posedge clk) begin
+		if(!rstn) begin
+			md_end_reg <= 0;
+		end
+		else begin
+			if(md_start) begin
+				num_reg <= num_in;
+				md_end_reg <= 1;
+			end
+			else begin
+				md_end_reg <= 0;
+			end
+		end
+	end
+
+	always @(*) begin
+		if(md_end_reg) begin
+			integer i;
+			integer pos = 0;
+			for(i = 63; i >= 0; i = i - 1) begin
+				if(num_reg[i] == 1) begin
+					pos = i + 1;
+				end
+			end
+			len_reg = pos;
+		end
+		else begin
+			len_reg = 0;
+		end
+	end
 
 endmodule
