844821911708f61c7c578ac748c5da2c9b998695
mightyking-ryu <dongwooryu@snu.ac.kr>
2024-12-02 19:20:00 +0900

    Implement long_div module

--- src/rtl/long_div.v
+++ src/rtl/long_div.v
@@ -16,6 +16,38 @@ output [31:0] ld_out;
 
 	//Implement long division module
 
+	reg md_end_reg;
+	reg [31:0] ld_out_reg;
+
+	assign md_end = md_end_reg;
+	assign ld_out = ld_out_reg;
+
+	reg next_md_end;
+	reg next_ld_out;
+
+	reg [95:0] dividend;
+	reg [31:0] remainder;
+
+	always @(posedge clk) begin
+		if(!rstn) begin
+			md_end_reg <= 0;
+			ld_out_reg <= 0;
+		end
+		else begin
+			md_end_reg <= next_md_end;
+			ld_out_reg <= next_ld_out;
+		end
+	end
+
+	always @(*) begin
+		if(md_start) begin
+			dividend = num_in << len;
+		end
+		else begin
+			next_md_end = 0;
+			next_ld_out = 0;
+		end
+	end
 
 
 endmodule
