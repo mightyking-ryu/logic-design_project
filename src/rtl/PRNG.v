64b921edb7fb27f3e8ab1432cd1dba8277d967ab
mightyking-ryu <dongwooryu@snu.ac.kr>
2024-12-02 12:36:41 +0900

    Implement pseudo_random module

--- src/rtl/PRNG.v
+++ src/rtl/PRNG.v
@@ -2,7 +2,28 @@
 
 module pseudo_random(input  clk, rstn, gen, input [31:0] N, output [31:0] q, output gen_end);
     //Implement pseudo random number generator
-    
-    
-     
+
+    reg [31:0] shift_reg;
+    reg gen_end_reg;
+
+    assign q = shift_reg;
+    assign gen_end = gen_end_reg;
+
+    always @(posedge clk) begin
+        if(!rstn) begin
+            shift_reg <= N;
+            gen_end_reg <= 0;
+        end
+        else begin
+            if(gen) begin
+                // Generate random number
+                shift_reg <= {shift_reg[30:0], shift_reg[31] ^ shift_reg[21] ^ shift_reg[1] ^ shift_reg[0]};
+                gen_end_reg <= 1;
+            end
+            else begin
+                gen_end_reg <= 0;
+            end
+        end
+    end
+
 endmodule
